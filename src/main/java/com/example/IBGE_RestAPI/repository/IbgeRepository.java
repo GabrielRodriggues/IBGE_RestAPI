c90ab20c9d1cf4804992f18799b38a0f3d4a8413
Gabriel da Costa Rodrigues <gabriellrodrigues2001@gmail.com>
2024-04-25 20:57:35 -0300

    Delete src directory

--- src/main/java/com/example/IBGE_RestAPI/repository/IbgeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.IBGE_RestAPI.repository;
-
-import com.example.IBGE_RestAPI.model.IbgeEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface IbgeRepository extends MongoRepository<IbgeEntity, String> {
-}
